26edf55749ebe46483f5d8f2e4d6c22e3f634f96
karolalencar <39858749+karolalencar@users.noreply.github.com>
2022-02-23 18:42:30 -0300

    Tabela motorista

---
+++ backend/db/postgres/taxon/schema/Motorista/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Motorista (
+    cpf VARCHAR(11),
+    data_nasc VARCHAR(10),
+    estado_civil VARCHAR(50),
+    rg VARCHAR(10),
+    renach VARCHAR(50),
+    agencia_bancaria VARCHAR(10),
+    conta_bancaria VARCHAR(10),
+    banco VARCHAR(50),
+    PRIMARY KEY (cpf),
+);
