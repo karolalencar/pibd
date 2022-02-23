35c6d17b651d843630e97638817d28baafe444c5
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-02-23 19:12:59 -0300

    typo id para chassi

--- backend/db/postgres/taxon/schema/Garagem/create.sql
+++ backend/db/postgres/taxon/schema/Garagem/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Garagem(
 	chassi VARCHAR(17),
 	garagem VARCHAR(255),
-	PRIMARY KEY (id, garagem),
+	PRIMARY KEY (chassi, garagem),
 	FOREIGN KEY (chassi) REFERENCES Veiculo(chassi)
 );
