4b8d7b2146f4c99de509c5fe6bb87fe8af520ae2
s4mukka <meninosam197@gmail.com>
2022-02-23 18:11:56 -0300

    veiculo

---
+++ backend/db/postgres/taxon/schema/Veiculo/create.sql
@@ -0,0 +1,10 @@
+CREATE Table `Veiculo`(
+	`chassi` VARCHAR(17),
+	`cor` VARCHAR(255),
+	`placa` VARCHAR(7),
+	`ano` INT,
+	`modelo` VARCHAR(50),
+	`max_ocupacao` INT,
+	`marca` VARCHAR(50),
+	PRIMARY KEY(`chassi`)
+);
