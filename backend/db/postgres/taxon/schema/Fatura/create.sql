9b66e1e04eaf11595a18ace4d8708f456165dbd7
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 20:45:24 -0300

    fatura situacao

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Fatura(
 	cnpj_conveniada CHAR(14),
-    mes VARCHAR(10) NOT NULL,
-    ano CHAR(4) NOT NULL,
+    mes VARCHAR(10),
+    ano CHAR(4),
+	situacao VARCHAR(20),
 	PRIMARY KEY(cnpj_conveniada, mes, ano),
 	FOREIGN KEY (cnpj_conveniada) REFERENCES Conveniada(cnpj)
 );
