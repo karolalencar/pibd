c36853791ddde267dddb558eb89c7bea99aff7e5
s4mukka <meninosam197@gmail.com>
2022-02-23 20:48:45 -0300

    proprietario-possui-veiculo

---
+++ backend/db/postgres/taxon/schema/ProprietarioPossuiVeiculo/create.sql
@@ -0,0 +1,7 @@
+CREATE Table ProprietarioPossuiVeiculo(
+	id INT,
+  chassi VARCHAR(17),
+	PRIMARY KEY(id, chassi),
+  FOREIGN KEY (id) REFERENCES Proprietario(id),
+  FOREIGN KEY (chassi) REFERENCES Veiculo(chassi)
+);
