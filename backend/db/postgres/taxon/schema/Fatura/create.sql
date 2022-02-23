764eeaf0e89ee972ac9ca020bb4789099c5a55c4
Ziron-hub <oaquila14@gmail.com>
2022-02-23 20:22:30 -0300

    Fatura

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Fatura(
-	cnpj_conveniada CHAR(14),
-    mes VARCHAR(3) NOT NULL,
-    ano VARCHAR(6) NOT NULL,
+	cnpj_conveniada CHAR(14) NOT NULL,
+    mes VARCHAR(10) NOT NULL,
+    ano CHAR(4) NOT NULL,
 	PRIMARY KEY(cnpj_conveniada, mes, ano),
 	FOREIGN KEY (cnpj_conveniada) REFERENCES Conveniada(cnpj)
 );
