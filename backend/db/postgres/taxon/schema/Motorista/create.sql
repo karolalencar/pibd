209ba96052a31a9bb26649a7ecb69b3d621cca1f
karolalencar <39858749+karolalencar@users.noreply.github.com>
2022-02-23 19:11:14 -0300

    Tabela atualizada

--- backend/db/postgres/taxon/schema/Motorista/create.sql
+++ backend/db/postgres/taxon/schema/Motorista/create.sql
@@ -1,8 +1,5 @@
 CREATE TABLE Motorista (
     cpf VARCHAR(11),
-    data_nasc VARCHAR(10),
-    estado_civil VARCHAR(50),
-    rg VARCHAR(10),
     renach VARCHAR(50),
     agencia_bancaria VARCHAR(10),
     conta_bancaria VARCHAR(10),
