6a099725dc57f33034cf4424be94bb0014fa51ef
Ziron-hub <oaquila14@gmail.com>
2022-02-23 20:04:30 -0300

    Conveniada

--- backend/db/postgres/taxon/schema/Convenio/create.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Convenio(
-	cnpj_conveniada CHAR(14),
-    convenio VARCHAR(30) NOT NULL,
-	PRIMARY KEY(cnpj_conveniada, convenio),
-	FOREIGN KEY (cnpj_conveniada) REFERENCES Conveniada(cnpj)
-);
