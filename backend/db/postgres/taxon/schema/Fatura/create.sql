06e257de4385dd9b3b523a57da3e4e8fc1bc29f5
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-02-23 20:36:15 -0300

    nao precisa do not null

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Fatura(
-	cnpj_conveniada CHAR(14) NOT NULL,
+	cnpj_conveniada CHAR(14),
     mes VARCHAR(10) NOT NULL,
     ano CHAR(4) NOT NULL,
 	PRIMARY KEY(cnpj_conveniada, mes, ano),
