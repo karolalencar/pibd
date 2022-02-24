48fa5f3e84854a58692663a1a33eacef1685038f
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 22:45:19 -0300

    fix agendamento nao se refere a local

--- backend/db/postgres/taxon/schema/Agendamento/create.sql
+++ backend/db/postgres/taxon/schema/Agendamento/create.sql
@@ -1,9 +1,6 @@
 create table Agendamento(
     cnpj VARCHAR(11),
-    cep VARCHAR(8), 
-    num INT,
     efetuado_as TIMESTAMP,
-    PRIMARY KEY(cnpj, cep, num, efetuado_as),
-    FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj),
-    FOREIGN KEY(cep, num) REFERENCES Local(cep, num)
+    PRIMARY KEY(cnpj, efetuado_as),
+    FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj)
 );
---
+++ backend/db/postgres/taxon/schema/Corrida/create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Corrida(
+	cpf CHAR(11),
+	chassi CHAR(17),
+	inicia_as TIMESTAMP,
+	termina_as TIMESTAMP,
+	valor REAL,
+	inicia_em VARCHAR(50),
+	termina_em VARCHAR(50),
+	agendamento_cnpj CHAR(14),
+	agendamento_efetuado_as TIMESTAMP,
+	fatura_cnpj CHAR(14),
+	fatura_mes VARCHAR(10),
+	fatura_ano CHAR(4),
+	PRIMARY KEY(cpf, chassi, inicia_as),
+	FOREIGN KEY(agendamento_cnpj, agendamento_efetuado_as) REFERENCES Agendamento(cnpj, efetuado_as),
+	FOREIGN KEY(fatura_cnpj, fatura_mes, fatura_ano) REFERENCES Fatura(cnpj, mes, ano)
+
+);
