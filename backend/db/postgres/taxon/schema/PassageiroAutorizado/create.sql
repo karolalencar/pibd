5f6bfa0274be61457b0cd5dc79136ab9d9679384
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-24 00:28:44 -0300

    fix pasageiro autorizado fk

--- backend/db/postgres/taxon/schema/PassageiroAutorizado/create.sql
+++ backend/db/postgres/taxon/schema/PassageiroAutorizado/create.sql
@@ -2,6 +2,5 @@ CREATE TABLE PassageiroAutorizado(
 	cnpj CHAR(14),
 	cpf CHAR(11),
 	PRIMARY KEY(cnpj, cpf),
-	FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj),
-	FOREIGN KEY(cpf) REFERENCES Passageiro(cpf)
+	FOREIGN KEY(cnpj, cpf) REFERENCES ConveniadaAutorizaPassageiro(cnpj, cpf)
 );
