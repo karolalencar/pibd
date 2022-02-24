70ff4e2dc889d343189556fed6261c26f57819fa
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 22:48:06 -0300

    fix agendamento nao refere a local

--- backend/db/postgres/taxon/schema/Agendamento/create.sql
+++ backend/db/postgres/taxon/schema/Agendamento/create.sql
@@ -2,5 +2,5 @@ create table Agendamento(
     cnpj VARCHAR(11),
     efetuado_as TIMESTAMP,
     PRIMARY KEY(cnpj, efetuado_as),
-    FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj)
+    FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj) 
 );
