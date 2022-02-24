9c2042a2d1d9b9f797a5bdbbf7a550e9e23e64e2
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 22:25:06 -0300

    datetime for timestamp to comply with pgsql

--- backend/db/postgres/taxon/schema/Agendamento/create.sql
+++ backend/db/postgres/taxon/schema/Agendamento/create.sql
@@ -2,7 +2,7 @@ create table Agendamento(
     cnpj VARCHAR(11),
     cep VARCHAR(8), 
     num INT,
-    efetuado_as DATETIME
+    efetuado_as TIMESTAMP,
     PRIMARY KEY(cnpj, cep, num, efetuado_as),
     FOREIGN KEY(cnpj) REFERENCES Conveniada(cnpj),
     FOREIGN KEY(cep, num) REFERENCES Local(cep, num)
