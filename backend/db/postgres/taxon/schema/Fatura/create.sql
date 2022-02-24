a74bc8a9c39185b9dc9006f67278229cc2e1d9e7
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 22:55:03 -0300

    fix fatura naming

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Fatura(
-	cnpj_conveniada CHAR(14),
+	cnpj CHAR(14),
     mes VARCHAR(10),
     ano CHAR(4),
 	situacao VARCHAR(20),
-	PRIMARY KEY(cnpj_conveniada, mes, ano),
-	FOREIGN KEY (cnpj_conveniada) REFERENCES Conveniada(cnpj)
+	PRIMARY KEY(cnpj, mes, ano),
+	FOREIGN KEY (cnpj) REFERENCES Conveniada(cnpj)
 );
