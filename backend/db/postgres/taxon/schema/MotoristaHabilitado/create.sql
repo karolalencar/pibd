f3b9c892c8a326082395fe1735c232315a28da0d
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-24 00:12:58 -0300

    fix fk to motorista acessa veiculo

--- backend/db/postgres/taxon/schema/MotoristaHabilitado/create.sql
+++ backend/db/postgres/taxon/schema/MotoristaHabilitado/create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE MotoristaHabilitado(
 	cpf CHAR(11),
 	chassi CHAR(17),
-	PRIMARY KEY(cpf, chassi)
+	PRIMARY KEY(cpf, chassi),
+	FOREIGN KEY(cpf, chassi) REFERENCES MotoristaAcessaVeiculo(cpf, chassi)
 );
