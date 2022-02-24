8305c502ec8a992fafb6e6f0eacf6586f11fd609
gsvboas <gsvboas@estudante.ufscar.br>
2022-02-23 21:20:47 -0300

    indent

--- backend/db/postgres/taxon/schema/ProprietarioPossuiVeiculo/create.sql
+++ backend/db/postgres/taxon/schema/ProprietarioPossuiVeiculo/create.sql
@@ -1,7 +1,7 @@
 CREATE Table ProprietarioPossuiVeiculo(
 	id INT,
-  chassi VARCHAR(17),
+  	chassi VARCHAR(17),
 	PRIMARY KEY(id, chassi),
-  FOREIGN KEY (id) REFERENCES Proprietario(id),
-  FOREIGN KEY (chassi) REFERENCES Veiculo(chassi)
+  	FOREIGN KEY (id) REFERENCES Proprietario(id),
+  	FOREIGN KEY (chassi) REFERENCES Veiculo(chassi)
 );
