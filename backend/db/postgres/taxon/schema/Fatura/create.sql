c79b9f0175e7f3df8d0143940b68aebac8398cc8
karolalencar <39858749+karolalencar@users.noreply.github.com>
2022-03-09 19:13:42 -0300

    check adicionado em situacao da fatura

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE Fatura(
 	cnpj CHAR(14),
     mes VARCHAR(10),
     ano CHAR(4),
-	situacao VARCHAR(20),
+	situacao VARCHAR(10) NOT NULL CHECK IN ('em aberto', 'em atraso', 'quitada'),
 	PRIMARY KEY(cnpj, mes, ano),
 	FOREIGN KEY (cnpj) REFERENCES Conveniada(cnpj)
 );
