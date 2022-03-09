047d69c4acda7b299d0e68eae2f95b389b386fe1
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 19:02:44 -0300

    Update create.sql

--- backend/db/postgres/taxon/schema/MotoristaCNH/create.sql
+++ backend/db/postgres/taxon/schema/MotoristaCNH/create.sql
@@ -1,7 +1,7 @@
 create table MotoristaCNH(
     cpf varchar(15),
     num_registro int,
-    categoria NOT NULL,
+    categoria VARCHAR(2) NOT NULL CHECK IN ('A', 'B', 'C', 'D', 'E', 'AB', 'AC', 'AD', 'AE'),
     vencimento_em TIMESTAMP NOT NULL,
     emitida_em TIMESTAMP NOT NULL, 
     PRIMARY key (cpf, num_registro),
