20207a3352319ff0bc180c5ee839ee67062bfb11
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 18:24:51 -0300

    not null attributes

--- backend/db/postgres/taxon/schema/TelefonePessoa/create.sql
+++ backend/db/postgres/taxon/schema/TelefonePessoa/create.sql
@@ -2,8 +2,8 @@ CREATE TABLE TelefonePessoa(
 	id INT, 
     	ddd INT,
     	numero int,
-    	tipo varchar(20),
-    	criado_em TIMESTAMP,
+    	tipo varchar(20) NOT NULL,
+    	criado_em TIMESTAMP NOT NULL,
     	PRIMARY key (id, ddd, numero),
     	FOREIGN key (id) REFERENCES PEssoa(id)
 );
