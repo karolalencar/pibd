210da3c1df684b5aad84348dbf2057e27f7d492b
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 21:18:26 -0300

    make fk to agendamento reference surrogate pk

--- backend/db/postgres/taxon/schema/PassageiroAutorizadoViajaEmAgendamentoPorLocal/create.sql
+++ backend/db/postgres/taxon/schema/PassageiroAutorizadoViajaEmAgendamentoPorLocal/create.sql
@@ -1,12 +1,11 @@
 CREATE TABLE PassageiroAutorizadoViajaEmAgendamentoPorLocal(
 	passageiro_autorizado_cpf CHAR(11),
 	passageiro_autorizado_cnpj CHAR(14),
-	agendamento_cnpj CHAR(14),
-	agendamento_efetuado_as TIMESTAMP,
+	agendamento_id INT,
 	cep CHAR(8),
 	num INT,
-	PRIMARY KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj, agendamento_cnpj, agendamento_efetuado_as),
+	PRIMARY KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj, agendamento_id),
 	FOREIGN KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj) REFERENCES PassageiroAutorizado(cpf, cnpj),
-	FOREIGN KEY (agendamento_cnpj, agendamento_efetuado_as) REFERENCES Agendamento(cnpj, efetuado_as),
+	FOREIGN KEY (agendamento_id) REFERENCES Agendamento(id),
 	FOREIGN KEY (cep, num) REFERENCES Local(cep, num)
 );
