8e20679c87cd97c66cd0b5eb7eaceb6037b7b938
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 21:07:11 -0300

    Update create.sql

--- backend/db/postgres/taxon/schema/Corrida/create.sql
+++ backend/db/postgres/taxon/schema/Corrida/create.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Corrida(
+	id SERIAL PRIMARY KEY,
 	cpf CHAR(11),
 	chassi CHAR(17),
 	inicia_as TIMESTAMP,
@@ -6,12 +7,9 @@ CREATE TABLE Corrida(
 	valor REAL,
 	inicia_em VARCHAR(50),
 	termina_em VARCHAR(50),
-	agendamento_cnpj CHAR(14),
-	agendamento_efetuado_as TIMESTAMP,
-	fatura_cnpj CHAR(14),
-	fatura_mes VARCHAR(10),
-	fatura_ano CHAR(4),
-	PRIMARY KEY(cpf, chassi, inicia_as),
+	agendamento_id INT NOT NULL,
+	fatura_id INT NOT NULL,
+	UNIQUE(cpf, chassi, inicia_as),
 	FOREIGN KEY(cpf, chassi) REFERENCES MotoristaHabilitado(cpf, chassi),
 	FOREIGN KEY(agendamento_cnpj, agendamento_efetuado_as) REFERENCES Agendamento(cnpj, efetuado_as),
 	FOREIGN KEY(fatura_cnpj, fatura_mes, fatura_ano) REFERENCES Fatura(cnpj, mes, ano)
