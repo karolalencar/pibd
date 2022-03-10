97b2a1f4ca781db341c57857fc44ec4584104916
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 21:19:04 -0300

    fix fks reference surrogate pks

--- backend/db/postgres/taxon/schema/buildscript.sql
+++ backend/db/postgres/taxon/schema/buildscript.sql
@@ -115,23 +115,21 @@ create table Agendamento(
 CREATE TABLE PassageiroAutorizadoViajaEmAgendamentoPorLocal(
 	passageiro_autorizado_cpf CHAR(11),
 	passageiro_autorizado_cnpj CHAR(14),
-	agendamento_cnpj CHAR(14),
-	agendamento_efetuado_as TIMESTAMP,
+	agendamento_id INT,
 	cep CHAR(8),
 	num INT,
-	PRIMARY KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj, agendamento_cnpj, agendamento_efetuado_as),
+	PRIMARY KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj, agendamento_id),
 	FOREIGN KEY (passageiro_autorizado_cpf, passageiro_autorizado_cnpj) REFERENCES PassageiroAutorizado(cpf, cnpj),
 	FOREIGN KEY (agendamento_id) REFERENCES Agendamento(id),
 	FOREIGN KEY (cep, num) REFERENCES Local(cep, num)
 );
 create table AgendamentoParaEmLocal(
-    cnpj VARCHAR(14),
-    efetuado_as TIMESTAMP,
+    agendamento_id INT,
     cep VARCHAR(8), 
     num INT,
     indice_trecho VARCHAR(255),
     num_passageiros INT,
-    PRIMARY KEY (cnpj, efetuado_as, cep, num),
+    PRIMARY KEY (agendamento_id, cep, num),
     FOREIGN KEY (agendamento_id) REFERENCES Agendamento(id),
     FOREIGN KEY (cep, num) REFERENCES Local(cep, num)
 );
