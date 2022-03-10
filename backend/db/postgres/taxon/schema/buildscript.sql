dc93fd836be44f092575b9c0a8c9896e13def204
gsvboas <77863706+gsvboas@users.noreply.github.com>
2022-03-09 21:10:19 -0300

    Update buildscript.sql

--- backend/db/postgres/taxon/schema/buildscript.sql
+++ backend/db/postgres/taxon/schema/buildscript.sql
@@ -96,11 +96,12 @@ CREATE TABLE PassageiroAutorizado(
 	FOREIGN KEY(cnpj, cpf) REFERENCES ConveniadaAutorizaPassageiro(cnpj, cpf)
 );
 CREATE TABLE Fatura(
+	id serial PRIMARY KEY,  -- surrogate PK
 	cnpj CHAR(14),
     	mes VARCHAR(10),
     	ano CHAR(4),
 	situacao VARCHAR(10) NOT NULL,
-	PRIMARY KEY(cnpj, mes, ano),
+	UNIQUE(cnpj, mes, ano),
 	FOREIGN KEY (cnpj) REFERENCES Conveniada(cnpj),
 	CHECK(situacao IN ('em aberto', 'em atraso', 'quitada'))
 );
