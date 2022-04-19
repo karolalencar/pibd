f8e2930e6a2f9756b6878ebde9c56b8e5c10a791
gustavo.jodar <gustavo.jodar@catijr.com.br>
2022-04-19 16:38:58 -0300

    arruma

--- backend/db/postgres/taxon/schema/Fatura/create.sql
+++ backend/db/postgres/taxon/schema/Fatura/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE Fatura(
 	cnpj CHAR(14),
     	data_criacao Date, 
 	situacao VARCHAR(10) NOT NULL,
-	UNIQUE(cnpj, mes, ano),
+	UNIQUE(data_criacao),
 	FOREIGN KEY (cnpj) REFERENCES Conveniada(cnpj),
 	CHECK(situacao IN ('em aberto', 'em atraso', 'quitada'))
 );
